cb3538452c54080d762d4d45c62394c3e425876e
Anmol Vaswani <anmolvaswani@gmail.com>
2025-05-01 13:46:25 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/payment')
+def payment():
+    return render_template('index.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
